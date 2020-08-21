7ed7ac243ed9f25a2bc7dc2eb4428b60fb1f4ff0
KJY <wpdud001@gmail.com>
2020-08-21 09:37:24 +0900

    CrossOrigin

--- sp19_Emp/src/main/java/com/encore/emp/controller/EmpolyeeController.java
+++ sp19_Emp/src/main/java/com/encore/emp/controller/EmpolyeeController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -23,6 +24,7 @@ import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin(origins= {"*"}, maxAge=6000)
 @Api(tags= {"Encore HRM"})
 public class EmpolyeeController {
 	
